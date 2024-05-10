599bef53ccc53456cef1bb4f1801085c6f24749c
darkpsinight <darkpsinight@hotmail.com>
2024-05-10 17:41:57 +0100

    setup + codes in progress

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,55 @@
 class SudokuSolver {
+  // convert row letters to numbers (to be used in "api.js" to get index of coordinate using extracted row and column values)
+  numberFromLetter(row) {
+    switch (row.toUpperCase()) {
+      case "A":
+        return 1;
+      case "B":
+        return 2;
+      case "C":
+        return 3;
+      case "D":
+        return 4;
+      case "E":
+        return 5;
+      case "F":
+        return 6;
+      case "G":
+        return 7;
+      case "H":
+        return 8;
+      case "I":
+        return 9;
+      default:
+        return "none";
+    }
+  }
 
+  // check puzzleString validity
   validate(puzzleString) {
+    // is missing puzzle, the returned value will be { error: 'Required field missing' }
+    if (!puzzleString) {
+      return "Required field missing";
+    }
+
+    //is greater or less than 81 characters, the returned value will be { error: 'Expected puzzle to be 81 characters long' }
+    if (puzzleString.length !== 81) {
+      return "Expected puzzle to be 81 characters long";
+    }
+
+    if (!/^[1-9]+$/.test(puzzleString)) {
+      return "Invalid characters";
+    }
+    return "Valid puzzle string";
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,64 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
+const { numberFromLetter } = require("../helpers/helper.js");
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    const { puzzleString, coordinate, value } = req.body;
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    if (!puzzleString || !coordinate || !value) {
+      return res.json({ error: "Required field(s) missing" });
+    }
 
-    });
+    if (solver.validate(puzzleString) !== "Valid puzzle string") {
+      return res.json({ error: solver.validate(puzzleString) });
+    }
+
+    // extract row and column values from coordinate string
+    const row = coordinate.split("")[0];
+    const column = coordinate.split("")[1];
+
+    // check coordinate validity
+    //If the coordinate submitted to api/check does not point to an existing grid cell, the returned value will be { error: 'Invalid coordinate'}
+    if (
+      !/[a-i]/i.test(row) ||
+      !/[1-9]/i.test(column) ||
+      coordinate.length !== 2
+    ) {
+      return res.json({ error: "Invalid coordinate" });
+    }
+
+    // check value validity
+    // If the value submitted to /api/check is not a number between 1 and 9, the returned value will be { error: 'Invalid value' }
+    if (!/^[1-9]$/.test(value)) {
+      return res.json({ error: "Invalid value" });
+    }
+
+    //get index of coordinate using extracted row and column values
+    const rowNumber = solver.numberFromLetter(row);
+    const columnIndex = parseInt(column, 10);
+    const index = (rowNumber - 1) * 9 + columnIndex - 1;
+
+    // checks if a value at a specific position (index) matches the expected value.
+    if (puzzleString[index] == value) {
+      return res.json({ valid: true });
+    }
+
+    //...
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    const { puzzleString, coordinate, value } = req.body;
+
+    if (!puzzleString || !coordinate || !value) {
+      return res.json({ error: "Required field missing" });
+    }
+
+    if (solver.validate(puzzleString) !== "Valid puzzle string") {
+      return res.json({ error: solver.validate(puzzleString) });
+    }
+  });
 };
